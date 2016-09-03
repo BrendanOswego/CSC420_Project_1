2ae4515e650bb726bcd7d5158e4bace376098cba
brendan <bgoldber@oswego.edu>
2016-09-02 20:28:13 -0400

    Check in

--- src/com/example/mainpackage/Main.java
+++ src/com/example/mainpackage/Main.java
@@ -4,4 +4,9 @@ package com.example.mainpackage;
  * Created by brendan on 9/2/16.
  */
 public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
 }
