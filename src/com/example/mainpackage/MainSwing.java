04ece222e5ffa538f6f3679c223151d4af578be9
brendan <bgoldber@oswego.edu>
2016-09-02 20:40:43 -0400

    Added git ignore

--- src/com/example/mainpackage/MainSwing.java
+++ src/com/example/mainpackage/MainSwing.java
@@ -1,7 +1,27 @@
 package com.example.mainpackage;
 
+import javax.swing.*;
+
 /**
  * Created by brendan on 9/2/16.
  */
 public class MainSwing {
+    private JCheckBox checkBox1;
+    private JPanel panel1;
+
+    public JCheckBox getCheckBox1() {
+        return checkBox1;
+    }
+
+    public void setCheckBox1(JCheckBox checkBox1) {
+        this.checkBox1 = checkBox1;
+    }
+
+    public JPanel getPanel1() {
+        return panel1;
+    }
+
+    public void setPanel1(JPanel panel1) {
+        this.panel1 = panel1;
+    }
 }
