17e3f3ea0201161e98a286a7f965487e219a0a2a
brendan <bgoldber@oswego.edu>
2016-09-02 21:25:26 -0400

    Checking to see if out is needed

--- src/com/example/mainpackage/Main.java
+++ src/com/example/mainpackage/Main.java
@@ -7,13 +7,7 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello World");
-        for (int i = 0;i < 5;i ++){
-            System.out.println(i);
-        }
-        for (int i = 0;i < 6;i++){
-            System.out.println(i);
-        }
-
+        System.out.println("Test");
     }
 
 }
