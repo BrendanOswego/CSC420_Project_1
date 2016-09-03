442eba8364e1a75f0ff59bec433f3d5e144b6061
brendan <bgoldber@oswego.edu>
2016-09-02 21:20:29 -0400

    testing _1

--- src/com/example/mainpackage/Main.java
+++ src/com/example/mainpackage/Main.java
@@ -10,6 +10,9 @@ public class Main {
         for (int i = 0;i < 5;i ++){
             System.out.println(i);
         }
+        for (int i = 0;i < 6;i++){
+            System.out.println(i);
+        }
 
     }
 
