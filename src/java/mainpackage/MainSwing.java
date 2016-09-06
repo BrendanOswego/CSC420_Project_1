40a7e6742005ad717a4ffe1727fab4f8fc9a2b26
brendan <bgoldber@oswego.edu>
2016-09-06 15:47:40 -0400

    Check in: Added picture methods and basic panel for music player

--- src/java/mainpackage/MainSwing.java
+++ src/java/mainpackage/MainSwing.java
@@ -1,5 +1,7 @@
 package mainpackage;
 
+import sun.jvm.hotspot.utilities.WorkerThread;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -17,6 +19,9 @@ public class MainSwing {
     private JLabel play;
     private JLabel pause;
     private JLabel refresh;
+    private JLabel currentTime;
+    private JLabel totalTime;
+
 
     public MainSwing() {
         createDesign();
@@ -26,34 +31,37 @@ public class MainSwing {
         MainSwing swing = new MainSwing();
     }
 
-    private void createDesign(){
+    private void createDesign() {
         JFrame jFrame = new JFrame();
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        jFrame.setSize(600,600);
+        jFrame.setSize(600, 600);
 
         JPanel infoPanel = new JPanel();
         JPanel mainPanel = new JPanel();
         JPanel soundControlPanel = new JPanel();
         JSlider musicSlider = new JSlider(JSlider.HORIZONTAL);
+        musicSlider.setValue(0);
         play = new JLabel();
         pause = new JLabel();
         refresh = new JLabel();
 
+        currentTime = new JLabel("0:00");
+        totalTime = new JLabel("Set Time");
 
-        BoxLayout verticalLayout = new BoxLayout(infoPanel,BoxLayout.Y_AXIS);
+        BoxLayout verticalLayout = new BoxLayout(infoPanel, BoxLayout.Y_AXIS);
         BorderLayout mainLayout = new BorderLayout();
         FlowLayout playerLayout = new FlowLayout();
 
         infoPanel.setLayout(verticalLayout);
-        infoPanel.setPreferredSize(new Dimension(jFrame.getWidth(),150));
+        infoPanel.setPreferredSize(new Dimension(jFrame.getWidth(), 150));
         infoPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 
         mainPanel.setLayout(mainLayout);
-        mainPanel.setSize(jFrame.getWidth(),jFrame.getHeight());
+        mainPanel.setSize(jFrame.getWidth(), jFrame.getHeight());
         mainPanel.setPreferredSize(mainPanel.getPreferredSize());
 
         soundControlPanel.setLayout(playerLayout);
-        soundControlPanel.setSize(600,20);
+        soundControlPanel.setSize(100, 2);
         soundControlPanel.setPreferredSize(soundControlPanel.getSize());
 
         title = new JLabel("Title");
@@ -62,31 +70,32 @@ public class MainSwing {
         artwork = new JLabel();
         title.setFont(title.getFont().deriveFont(15f));
         artist.setFont(artist.getFont().deriveFont(15f));
-        artist.setBorder(BorderFactory.createEmptyBorder(10,0,0,0));
-        artwork.setBorder(BorderFactory.createEmptyBorder(10,0,10,0));
-        refresh.setBorder(BorderFactory.createEmptyBorder(0,10,0,0));
+        artist.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
+        artwork.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
+        refresh.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
 
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
         artist.setAlignmentX(Component.CENTER_ALIGNMENT);
         artwork.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        createIcon(artwork,"oswego_icon",70,70);
-        createIconPNG(play,"play_button",20,20);
-        createIconPNG(pause,"pause_button",20,20);
-        createIconPNG(refresh,"replay_button",20,20);
-
+        createIcon(artwork, "oswego_icon", 70, 70);
+        createIconPNG(play, "play_button", 20, 20);
+        createIconPNG(pause, "pause_button", 20, 20);
+        createIconPNG(refresh, "replay_button", 20, 20);
 
         infoPanel.add(artwork);
         infoPanel.add(title);
         infoPanel.add(artist);
 
         soundControlPanel.add(play);
+        soundControlPanel.add(currentTime);
         soundControlPanel.add(musicSlider);
+        soundControlPanel.add(totalTime);
         soundControlPanel.add(pause);
         soundControlPanel.add(refresh);
 
-        mainPanel.add(infoPanel,BorderLayout.NORTH);
-        mainPanel.add(soundControlPanel,BorderLayout.CENTER);
+        mainPanel.add(infoPanel, BorderLayout.NORTH);
+        mainPanel.add(soundControlPanel, BorderLayout.CENTER);
 
         jFrame.setContentPane(mainPanel);
         jFrame.setVisible(true);
@@ -106,11 +115,11 @@ public class MainSwing {
 
     }
 
-    private void createIcon(JLabel label,String name,int width,int height) {
+    private void createIcon(JLabel label, String name, int width, int height) {
         //Uses above method and sets the icon to the local JLabel
         ImageIcon icon = findImagePath("/images/" + name + ".gif");
         Image image = icon.getImage();
-        Image scaledImage = image.getScaledInstance(width,height,Image.SCALE_SMOOTH);
+        Image scaledImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
         icon = new ImageIcon(scaledImage);
         label.setIcon(icon);
         if (icon != null) {
@@ -119,11 +128,12 @@ public class MainSwing {
             System.out.println("Image not found");
         }
     }
-    private void createIconPNG(JLabel label,String name,int width,int height) {
+
+    private void createIconPNG(JLabel label, String name, int width, int height) {
         //Uses above method and sets the icon to the local JLabel
         ImageIcon icon = findImagePath("/images/" + name + ".png");
         Image image = icon.getImage();
-        Image scaledImage = image.getScaledInstance(width,height,Image.SCALE_SMOOTH);
+        Image scaledImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
         icon = new ImageIcon(scaledImage);
         label.setIcon(icon);
         if (icon != null) {
@@ -134,8 +144,9 @@ public class MainSwing {
     }
 
 
-
-
+    public void setTitle(String title) {
+        this.title.setText(title);
+    }
 
 
 }
