155fb7fd54764b58e072be1a9401419af046d8e9
brendan <bgoldber@oswego.edu>
2016-09-06 17:00:06 -0400

    Added layouts for west and east sides and added temp layout of dynamic JLables in center

--- src/java/mainpackage/MainSwing.java
+++ src/java/mainpackage/MainSwing.java
@@ -7,6 +7,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Created by brendan on 9/2/16.
@@ -21,6 +22,8 @@ public class MainSwing {
     private JLabel refresh;
     private JLabel currentTime;
     private JLabel totalTime;
+    private ArrayList<String> libraryList;
+    private ArrayList<JLabel> songLabels;
 
 
     public MainSwing() {
@@ -34,11 +37,35 @@ public class MainSwing {
     private void createDesign() {
         JFrame jFrame = new JFrame();
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        jFrame.setSize(600, 600);
+        jFrame.setSize(700, 700);
 
         JPanel infoPanel = new JPanel();
         JPanel mainPanel = new JPanel();
+        JPanel westPanel = new JPanel();
+        JPanel centerPanel = new JPanel();
+        centerPanel.setPreferredSize(new Dimension(100, 100));
+        westPanel.setPreferredSize(new Dimension(120, jFrame.getHeight()));
+        JPanel eastPanel = new JPanel();
+        eastPanel.setPreferredSize(new Dimension(120, jFrame.getHeight()));
         JPanel soundControlPanel = new JPanel();
+
+        JLabel libraryHeader = new JLabel("Library");
+        JLabel artistHeader = new JLabel("Artist");
+
+        libraryList = new ArrayList<>();
+        libraryList.add("Test");
+        libraryList.add("Another One");
+        libraryList.add("Damn Daniel");
+        for (String aLibraryList : libraryList) {
+            JLabel label = new JLabel(aLibraryList);
+            label.setBorder(BorderFactory.createEmptyBorder(2,0,2,0));
+            centerPanel.add(label);
+            centerPanel.revalidate();
+            centerPanel.repaint();
+        }
+        libraryHeader.setAlignmentX(Component.CENTER_ALIGNMENT);
+        artistHeader.setAlignmentX(Component.CENTER_ALIGNMENT);
+
         JSlider musicSlider = new JSlider(JSlider.HORIZONTAL);
         musicSlider.setValue(0);
         play = new JLabel();
@@ -48,21 +75,25 @@ public class MainSwing {
         currentTime = new JLabel("0:00");
         totalTime = new JLabel("Set Time");
 
+        BoxLayout centerLayout = new BoxLayout(centerPanel, BoxLayout.Y_AXIS);
         BoxLayout verticalLayout = new BoxLayout(infoPanel, BoxLayout.Y_AXIS);
         BorderLayout mainLayout = new BorderLayout();
         FlowLayout playerLayout = new FlowLayout();
-
+        BoxLayout westLayout = new BoxLayout(westPanel, BoxLayout.Y_AXIS);
+        BoxLayout eastLayout = new BoxLayout(eastPanel, BoxLayout.Y_AXIS);
         infoPanel.setLayout(verticalLayout);
-        infoPanel.setPreferredSize(new Dimension(jFrame.getWidth(), 150));
-        infoPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        infoPanel.setPreferredSize(new Dimension(jFrame.getWidth(), 140));
+        //infoPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 
         mainPanel.setLayout(mainLayout);
-        mainPanel.setSize(jFrame.getWidth(), jFrame.getHeight());
-        mainPanel.setPreferredSize(mainPanel.getPreferredSize());
+        mainPanel.setPreferredSize(new Dimension(jFrame.getWidth(), jFrame.getHeight()));
 
         soundControlPanel.setLayout(playerLayout);
-        soundControlPanel.setSize(100, 2);
-        soundControlPanel.setPreferredSize(soundControlPanel.getSize());
+        soundControlPanel.setPreferredSize(new Dimension(100, 10));
+
+        westPanel.setLayout(westLayout);
+        eastPanel.setLayout(eastLayout);
+        centerPanel.setLayout(centerLayout);
 
         title = new JLabel("Title");
         artist = new JLabel("Artist");
@@ -70,15 +101,16 @@ public class MainSwing {
         artwork = new JLabel();
         title.setFont(title.getFont().deriveFont(15f));
         artist.setFont(artist.getFont().deriveFont(15f));
-        artist.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
-        artwork.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
-        refresh.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
+
+        artwork.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0));
+        //westPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        //eastPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
         artist.setAlignmentX(Component.CENTER_ALIGNMENT);
         artwork.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        createIcon(artwork, "oswego_icon", 70, 70);
+        createIcon(artwork, "oswego_icon", 50, 50);
         createIconPNG(play, "play_button", 20, 20);
         createIconPNG(pause, "pause_button", 20, 20);
         createIconPNG(refresh, "replay_button", 20, 20);
@@ -94,11 +126,24 @@ public class MainSwing {
         soundControlPanel.add(pause);
         soundControlPanel.add(refresh);
 
+        infoPanel.add(soundControlPanel);
+
+        westPanel.add(libraryHeader);
+        eastPanel.add(artistHeader);
+
+        centerPanel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+
         mainPanel.add(infoPanel, BorderLayout.NORTH);
-        mainPanel.add(soundControlPanel, BorderLayout.CENTER);
+        mainPanel.add(centerPanel, BorderLayout.CENTER);
+        mainPanel.add(westPanel, BorderLayout.LINE_START);
+        mainPanel.add(eastPanel, BorderLayout.LINE_END);
 
         jFrame.setContentPane(mainPanel);
         jFrame.setVisible(true);
+
+    }
+
+    private void setUpLibrary() {
 
     }
 
